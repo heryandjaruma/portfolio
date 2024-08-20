20f70bcf6b47a795c7f8e04deda800aa1c263c0e
Heryan Djaruma <heryandjaruma@gmail.com>
2024-08-20 15:41:06 +0700

    update award

--- src/app/sections/DaAward.tsx
+++ src/app/sections/DaAward.tsx
@@ -86,6 +86,49 @@ export default function DaAward() {
                     priority
                   />
                   <Image
+                    src={`/assets/image/third_place.png`}
+                    width={200}
+                    height={200}
+                    alt="cap"
+                    className="absolute w-5 -right-1 -top-1"
+                    priority
+                  />
+                  <p className={SansFransBold.className}>
+                    
+                    <span className={SansFrans.className}>
+                      3rd Place - hackjakarta 2024
+                    </span>
+                  </p>
+                  <p className={SansFransBold.className}>
+                    by{" "}
+                    <span className={SansFrans.className}>
+                      Grab, hackglobal
+                    </span>
+                  </p>
+                  <p
+                    className={`w-full text-end text-xs ${SansFransBold.className} text-gray-400`}
+                  >
+                    Received 7/24
+                  </p>
+                  <Image
+                    src={`/assets/image/bubble_tail.png`}
+                    width={200}
+                    height={200}
+                    alt="cap"
+                    className="absolute w-5 -left-[7px] bottom-0 -z-10"
+                    priority
+                  />
+                </div>
+                <div className=" bg-gray-50 px-3 pt-2 pb-1 ml-3 mr-5 rounded-xl -translate-x-1 text-sm relative">
+                  <Image
+                    src={`/assets/image/reaction_bubble.png`}
+                    width={200}
+                    height={200}
+                    alt="cap"
+                    className="absolute w-10 -right-4 -top-3"
+                    priority
+                  />
+                  <Image
                     src={`/assets/image/first_place.png`}
                     width={200}
                     height={200}
@@ -94,13 +137,13 @@ export default function DaAward() {
                     priority
                   />
                   <p className={SansFransBold.className}>
-                    Status:{" "}
+                    
                     <span className={SansFrans.className}>
                       Global Nomination 1 - 2023 Space Apps Challenge
                     </span>
                   </p>
                   <p className={SansFransBold.className}>
-                    Giver:{" "}
+                    by{" "}
                     <span className={SansFrans.className}>
                       NASA Space Apps Jakarta
                     </span>
@@ -137,13 +180,13 @@ export default function DaAward() {
                     priority
                   />
                   <p className={SansFransBold.className}>
-                    Status:{" "}
+                    
                     <span className={SansFrans.className}>
                       Awardee - Global Korea Scholarship (GKS) Non-degree
                     </span>
                   </p>
                   <p className={SansFransBold.className}>
-                    Giver:{" "}
+                    by{" "}
                     <span className={SansFrans.className}>
                       Korea Ministry of Education
                     </span>
@@ -180,13 +223,13 @@ export default function DaAward() {
                     priority
                   />
                   <p className={SansFransBold.className}>
-                    Status:{" "}
+                    
                     <span className={SansFrans.className}>
                       1st Winner - Hackathon SoCS
                     </span>
                   </p>
                   <p className={SansFransBold.className}>
-                    Giver:{" "}
+                    by{" "}
                     <span className={SansFrans.className}>
                       School of Computer Science Binus University
                     </span>
@@ -223,13 +266,13 @@ export default function DaAward() {
                     priority
                   />
                   <p className={SansFransBold.className}>
-                    Status:{" "}
+                    
                     <span className={SansFrans.className}>
                       1st Winner - Techfest Exhibition Competition
                     </span>
                   </p>
                   <p className={SansFransBold.className}>
-                    Giver:{" "}
+                    by{" "}
                     <span className={SansFrans.className}>
                       HIMTI Binus University
                     </span>
@@ -266,13 +309,13 @@ export default function DaAward() {
                     priority
                   />
                   <p className={SansFransBold.className}>
-                    Status:{" "}
+                    
                     <span className={SansFrans.className}>
                       Awardee - Beasiswa SejutaCita Batch 1
                     </span>
                   </p>
                   <p className={SansFransBold.className}>
-                    Giver:{" "}
+                    by{" "}
                     <span className={SansFrans.className}>
                       SejutaCita Indonesia
                     </span>
@@ -309,13 +352,13 @@ export default function DaAward() {
                     priority
                   />
                   <p className={SansFransBold.className}>
-                    Status:{" "}
+                    
                     <span className={SansFrans.className}>
                       Winner - Friendship Without Barriers Poster Competition
                     </span>
                   </p>
                   <p className={SansFransBold.className}>
-                    Giver:{" "}
+                    by{" "}
                     <span className={SansFrans.className}>
                       Friends Care Community (FCC) Binus University
                     </span>
@@ -352,14 +395,14 @@ export default function DaAward() {
                     priority
                   />
                   <p className={SansFransBold.className}>
-                    Status:{" "}
+                    
                     <span className={SansFrans.className}>
                       Participant - International Community Development Program
                       (ICDP)
                     </span>
                   </p>
                   <p className={SansFransBold.className}>
-                    Giver:{" "}
+                    by{" "}
                     <span className={SansFrans.className}>
                       Binus University
                     </span>
